9d8a800c5483827e04048736414e42acbf7143d2
Nandhana Sakthivel <nsakthi1@login01.g100.cineca.it>
2024-09-05 11:31:46 +0200

    Changes in CPU nominal frequency

--- src/tool.c
+++ src/tool.c
@@ -614,32 +614,45 @@ HIDDEN void get_rand_postfix(char *postfix, int size)
 #ifdef INTEL
 HIDDEN int read_intel_nom_freq()
 {
-	FILE *fd;
-	char *result;
-	float nom_freq;
+    FILE *fd;
+    char *result;
+    float nom_freq;
+   
+    fd = fopen("/sys/devices/system/cpu/cpu0/cpufreq/base_frequency", "r");
+    if(fd != NULL)
+    {
+            fscanf(fd, "%f", &nom_freq);
+            nom_freq /= pow(10,3);
+    }
+    else
+    {
+            fd = fopen("/proc/cpuinfo", "r");
+            if(fd != NULL)
+            {
+                size_t n = 0;
+                char *line = NULL;
+                while(getline(&line, &n, fd) > 0)
+                {
+                        if(!strncmp(line, "model name", 10))
+                        {
+                            result = strtok(line, ":");
+                            result = strtok(NULL, "@");
+                            result = strtok(NULL, "@");
+                            sscanf(result, " %fGHz", &nom_freq);
+                            break;
+                        }
+                }
+                free(line);
+                fclose(fd);
+                nom_freq *= 1000;
+            }
+            else
+            {   
+                fprintf(stderr, "Error: <COUNTDOWN-node:%s-rank:%d> Failed to read file: /proc/cpuinfo\n", cntd->node.hostname, cntd->rank->world_rank);
+                PMPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
 
-	fd = fopen("/proc/cpuinfo", "r");
-	if (fd == NULL) {
-		fprintf(stderr,
-			"Error: <COUNTDOWN-node:%s-rank:%d> Failed to read file: /proc/cpuinfo\n",
-			cntd->node.hostname, cntd->rank->world_rank);
-		PMPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-	}
-
-	size_t n = 0;
-	char *line = NULL;
-	while (getline(&line, &n, fd) > 0) {
-		if (!strncmp(line, "model name", 10)) {
-			result = strtok(line, ":");
-			result = strtok(NULL, "@");
-			result = strtok(NULL, "@");
-			sscanf(result, " %fGHz", &nom_freq);
-			break;
-		}
-	}
-	free(line);
-	fclose(fd);
-
-	return (int)(nom_freq * 1000);
+            }
+    }       
+    return (int) (nom_freq);
 }
 #endif
